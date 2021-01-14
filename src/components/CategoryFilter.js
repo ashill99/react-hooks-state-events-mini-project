917c10eaedd8bfb4c1d22ed3ba26d27ee6735b34
ashill99 <admshng@gmail.com>
2021-01-13 22:58:02 -0500

    commit tree 223d89ef81f600cbc7540c82420f9c96da4174a0

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+function CategoryFilter({onSelectCategory, selectedCategory, categories}) {
 
-function CategoryFilter() {
+  // const [isSelected, setIsSelected] = useState(false)
+ 
+  // function handleClassClick() {
+  //   setIsSelected(!isSelected);
+  //   return isSelected
+  // }
+const categoryButtons = categories.map((category) => {
+  const className = category === selectedCategory ? "selected" : null;
+  return (
+    <button 
+    key={category}
+    className={className}
+    onClick={() => onSelectCategory(category)}
+    >{category}</button>
+  )
+})
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {categoryButtons}
     </div>
   );
 }
